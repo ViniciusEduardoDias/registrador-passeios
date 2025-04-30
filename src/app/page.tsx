42bcbaff05392901207701501eb4479383a6c37c
Vinícius Dias <diasvinicius0401@gmail.com>
2025-04-30 10:57:03 -0300

    feat: First commit | structure of project; feat: Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,60 @@
+"use client";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { MdNotStarted } from "react-icons/md";
+import { TbRouteSquare2 } from "react-icons/tb";
+import { FaCheckSquare } from "react-icons/fa";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+  return (
+    <main className="flex flex-col gap-3 items-center justify-center min-h-screen bg-gray-950 text-white p-6">
+      <footer className="flex flex-col items-center">
+        <Image
+          src={"/Registrador.svg"}
+          alt="logo"
+          width={250}
+          height={250}
+        ></Image>
       </footer>
-    </div>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p className="text-lg text-center text-gray-300 mb-2">
+          Registre seus passeios, caminhadas ou pedaladas com distância, tempo e
+          trajeto!
+        </p>
+        <ul className="flex flex-col md:flex-row gap-2 mb-4">
+          <li className="flex items-center py-2 px-4 gap-2 border rounded-lg">
+            <FaCheckSquare />
+            Compare sua evolução
+          </li>
+          <li className="flex items-center py-2 px-4 gap-2 border rounded-lg">
+            <FaCheckSquare />
+            Coloque metas
+          </li>
+          <li className="flex items-center py-2 px-4 gap-2 border rounded-lg">
+            <FaCheckSquare />
+            Se motive
+          </li>
+        </ul>
+      </div>
+      <div className="w-full max-w-sm flex flex-col gap-4">
+        <button
+          onClick={() => router.push("/velocimetro")}
+          className="relative flex justify-center gap-3 items-center bg-green-500 hover:bg-green-600 text-white font-semibold py-3 rounded-2xl text-lg transition"
+        >
+          Iniciar Passeio
+          <MdNotStarted className="absolute right-3 size-6" />
+        </button>
+
+        <button
+          onClick={() => router.push("/passeios")}
+          className="relative flex justify-center gap-3 items-center border border-gray-400 hover:border-white text-gray-300 hover:text-white font-semibold py-3 rounded-2xl text-lg transition"
+        >
+          Ver Histórico
+          <TbRouteSquare2 className="absolute right-3 size-6 text-white" />
+        </button>
+      </div>
+    </main>
   );
 }
